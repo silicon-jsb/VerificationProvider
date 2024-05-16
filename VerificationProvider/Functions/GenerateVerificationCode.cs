6ac95922ca6fd4ac8ce1f34cc61466bb6793818b
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-05-16 11:01:31 +0200

    verification code complete

--- VerificationProvider/Functions/GenerateVerificationCode.cs
+++ VerificationProvider/Functions/GenerateVerificationCode.cs
@@ -1,104 +1,51 @@
-using System;
-using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.EntityFrameworkCore.Storage.Json;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using VerificationProvider.Models;
+using VerificationProvider.Services;
 
 namespace VerificationProvider.Functions;
 
-public class GenerateVerificationCode(ILogger<GenerateVerificationCode> logger, IServiceProvider serviceProvider)
+public class GenerateVerificationCode(ILogger<GenerateVerificationCode> logger, IServiceProvider serviceProvider, IVerificationService verificationService)
 {
-    private readonly ILogger<GenerateVerificationCode> _logger = logger;
+	private readonly ILogger<GenerateVerificationCode> _logger = logger;
 	private readonly IServiceProvider _serviceProvider = serviceProvider;
+	private readonly IVerificationService _verificationService = verificationService;
+
+
 
 	[Function(nameof(GenerateVerificationCode))]
 	[ServiceBusOutput("email_request", Connection = "ServiceBusConnection")]
-    public async Task Run([ServiceBusTrigger("verification_request", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message,ServiceBusMessageActions messageActions)
-    {
+	public async Task<string> Run([ServiceBusTrigger("verification_request", Connection = "ServiceBusConnection")] ServiceBusReceivedMessage message, ServiceBusMessageActions messageActions)
+	{
 		try
 		{
-			var verificationRequest = UnpackVerificationRquest(message);
+			var verificationRequest = _verificationService.UnpackVerificationRquest(message);
 			if (verificationRequest != null)
 			{
-				var code = GenerateCode();
+				var code = _verificationService.GenerateCode();
 				if (!string.IsNullOrEmpty(code))
 				{
-					var emailRequest = GenerateEmailRequest(verificationRequest, code);
+					var result = await _verificationService.SaveVerificationRequest(verificationRequest, code);
+					if (result)
+					{
+						var emailRequest = _verificationService.GenerateEmailRequest(verificationRequest, code);
+						if (emailRequest != null)
+						{
+							var payload = _verificationService.GenerateServiceBusEmailRequest(emailRequest);
+							if (!string.IsNullOrEmpty(payload))
+							{
+								await messageActions.CompleteMessageAsync(message);
+								return payload;
+							}
+						}
+					}
+
 				}
 			}
 		}
 		catch (Exception ex)
 		{
 			_logger.LogError($"GenerateVerificationCode.Run() :: {ex.Message}");
-		}
-	}
-
-	public VerificationRequest UnpackVerificationRquest(ServiceBusReceivedMessage message)
-	{
-		try
-		{
-			var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(message.Body.ToString());
-			if (verificationRequest != null && !string.IsNullOrEmpty(verificationRequest.Email))
-				return verificationRequest; 
-			
-		}
-		catch (Exception ex)
-		{
-			_logger.LogError($"GenerateVerificationCode.UnpackVerificationRquest :: {ex.Message}");
-		}
-		return null!;
-	}
-
-	public string GenerateCode()
-	{
-		try
-		{
-			var rnd = new Random();
-			var code = rnd.Next(100000, 999999);
-
-			return code.ToString();
-		}
-		catch (Exception ex)
-		{
-			_logger.LogError($"GenerateVerificationCode.GenerateCode :: {ex.Message}");
-		}
-		return null!;
-	}
-
-	private EmailRequest GenerateEmailRequest(VerificationRequest verificationRequest, string code)
-	{
-		try
-		{
-			if (!string.IsNullOrEmpty(verificationRequest.Email) && !string.IsNullOrEmpty(code))
-			{
-				var emailRequest = new EmailRequest()
-				{
-					To = verificationRequest.Email,
-					Subject = $"Verification Code {code}",
-					HtmlBody = $@"
-					<html lang='en'>
-						<head>
-							<meta charset='UTF-8'>
-							<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-							<title>Verification Code</title>
-						</head>
-						<body>
-						<div style='color: #191919; max-width: 500px'>
-							<h1 style='font-weight: 400;'>Verification Code</h1>
-						</div>
-
-		
-
-					"
-				}
-			}
-		}
-		catch (Exception ex)
-		{
-			_logger.LogError($"GenerateVerificationCode.GenerateEmailRequest :: {ex.Message}");
 		}
 		return null!;
 	}
