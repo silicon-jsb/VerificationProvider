16933f7014f2731ab826dc8d524053e984440ebc
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-05-16 13:55:36 +0200

    Working

---
+++ VerificationProvider/Functions/VerificationCleaner.cs
@@ -0,0 +1,28 @@
+using System;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using VerificationProvider.Services;
+
+namespace VerificationProvider.Functions
+{
+	public class VerificationCleaner(ILogger<VerificationCleaner> logger, IVerificationCleanerService verificationCleanerService)
+	{
+		private readonly ILogger<VerificationCleaner> _logger = logger;
+		private readonly IVerificationCleanerService _verificationCleanerService = verificationCleanerService;
+
+	
+
+		[Function("VerificationCleaner")]
+		public async Task Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
+		{
+			try
+			{
+				await _verificationCleanerService.RemoveExpiredRecordsAsync();
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"VerificationCleaner.Run() :: {ex.Message}");
+			}
+		}
+	}
+}
